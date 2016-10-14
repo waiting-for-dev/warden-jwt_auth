867743add7fb82d9e88a4402940cf47625321c84
Marc Busqué <marc@lamarciana.com>
2016-10-14 12:13:56 +0200

    Remove dummy test

--- spec/devise/jwt_spec.rb
+++ spec/devise/jwt_spec.rb
@@ -6,8 +6,4 @@ describe Devise::Jwt do
   it 'has a version number' do
     expect(Devise::Jwt::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    # expect(false).to eq(true)
-  end
 end
