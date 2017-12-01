f267e90291a57248ab86f9a156eecf3afba4f568
Ty Cobb <ty.cobb.m@gmail.com>
2017-12-01 10:50:25 -0600

    satisfy cops

--- spec/warden/jwt_auth_spec.rb
+++ spec/warden/jwt_auth_spec.rb
@@ -7,32 +7,34 @@ describe Warden::JWTAuth do
     expect(Warden::JWTAuth::VERSION).not_to be nil
   end
 
-  describe '#config' do
-    describe '#mappings' do
-      context 'when the mapping is a constant name' do
-        it 'resolves to the constant' do
-          described_class.configure do |config|
-            config.mappings = { user: 'Fixtures::UserRepo' }
-          end
-
-          expect(described_class.config.mappings).to eq({
-            user: Fixtures::UserRepo
-          })
+  describe '#config.mappings' do
+    context 'when the mapping is a constant name' do
+      before do
+        described_class.configure do |config|
+          config.mappings = { user: 'Fixtures::UserRepo' }
         end
       end
+
+      it 'resolves to the constant' do
+        expect(described_class.config.mappings).to eq(
+          user: Fixtures::UserRepo
+        )
+      end
     end
+  end
 
-    describe '#revocation_strategies' do
-      context 'when the mapping is a constant name' do
-        it 'resolves to the constant' do
-          described_class.configure do |config|
-            config.revocation_strategies = { user: 'Fixtures::RevocationStrategy' }
-          end
-
-          expect(described_class.config.revocation_strategies).to eq({
-            user: Fixtures::RevocationStrategy
-          })
+  describe '#config.revocation_strategies' do
+    context 'when the mapping is a constant name' do
+      before do
+        described_class.configure do |config|
+          config.revocation_strategies = { user: 'Fixtures::RevocationStrategy' }
         end
+      end
+
+      it 'resolves to the constant' do
+        expect(described_class.config.revocation_strategies).to eq(
+          user: Fixtures::RevocationStrategy
+        )
       end
     end
   end
