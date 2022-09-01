5454dfc46a80ea78ebf7c6987131c529e44846c7
Fabien Loup <fabien@norydev.com>
2022-09-01 16:06:00 +0200

    Support asymmetric algorithms
    For asymetric algorithms, the decoding secret is different than the
    encoding secret. This change adds support for an optional decoding
    secret configuration.

--- spec/support/shared_contexts/configuration.rb
+++ spec/support/shared_contexts/configuration.rb
@@ -4,6 +4,8 @@ shared_context 'configuration' do
   before do
     Warden::JWTAuth.configure do |config|
       config.secret = '123'
+      config.decoding_secret = '123'
+      config.algorithm = 'HS256'
       config.dispatch_requests = [['POST', %r{^/sign_in$}]]
       config.revocation_requests = [['DELETE', %r{^/sign_out$}]]
       config.revocation_strategies = { user: Fixtures::RevocationStrategy.new }
@@ -14,6 +16,8 @@ shared_context 'configuration' do
 
   let(:config) { Warden::JWTAuth.config }
   let(:secret) { config.secret }
+  let(:decoding_secret) { config.decoding_secret }
+  let(:algorithm) { config.algorithm }
   let(:dispatch_requests) { config.dispatch_requests }
   let(:revocation_requests) { config.revocation_requests }
   let(:revocation_strategies) { config.revocation_strategies }
