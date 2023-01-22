1e5e84e2d1f42016370fc971ec3b4c30bb0a7d47
Jouni Renfors <jouni@renfors.net>
2023-01-22 18:23:38 +0200

    Rubocop autocorrects

--- lib/warden/jwt_auth/token_decoder.rb
+++ lib/warden/jwt_auth/token_decoder.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require "jwt/error"
+require 'jwt/error'
 
 module Warden
   module JWTAuth
@@ -16,19 +16,17 @@ module Warden
       # @param token [String] a JWT
       # @return [Hash] payload decoded from the JWT
       def call(token)
-        begin
-          JWT.decode(token,
-                     decoding_secret,
-                     true,
-                     algorithm: algorithm,
-                     verify_jti: true)[0]
-        rescue JWT::VerificationError => e
-          JWT.decode(token,
-                     rotation_secret,
-                     true,
-                     algorithm: algorithm,
-                     verify_jti: true)[0]
-        end
+        JWT.decode(token,
+                   decoding_secret,
+                   true,
+                   algorithm: algorithm,
+                   verify_jti: true)[0]
+      rescue JWT::VerificationError => e
+        JWT.decode(token,
+                   rotation_secret,
+                   true,
+                   algorithm: algorithm,
+                   verify_jti: true)[0]
       end
     end
   end
