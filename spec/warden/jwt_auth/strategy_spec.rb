ca73fa1b58d29c6f4bd61b0541913dfb743c1adc
Marc Busqué <marc.busque@remote.com>
2024-12-20 11:48:35 +0100

    Reorganize strategy tests

--- spec/warden/jwt_auth/strategy_spec.rb
+++ spec/warden/jwt_auth/strategy_spec.rb
@@ -14,60 +14,28 @@ describe Warden::JWTAuth::Strategy do
 
   describe '#valid?' do
     context 'when Authorization header is valid' do
-      it 'returns true' do
-        env = { 'HTTP_AUTHORIZATION' => 'Bearer 123', 'PATH_INFO' => '/users', 'REQUEST_METHOD' => 'GET' }
+      it "returns true when it doesn't match a dispatch request and issuer claim is not present in the token" do
+        env = { 'HTTP_AUTHORIZATION' => 'Bearer 123' }
         strategy = described_class.new(env, :user)
 
         expect(strategy).to be_valid
       end
 
-      it 'returns false when the current path / method matches a dispatch request path / method' do
+      it 'returns false when the current path & method match a dispatch request' do
         env = { 'HTTP_AUTHORIZATION' => 'Bearer 123', 'PATH_INFO' => '/sign_in', 'REQUEST_METHOD' => 'POST' }
         strategy = described_class.new(env, :user)
 
         expect(strategy).not_to be_valid
       end
 
-      it 'returns true when the current path matches a dispatch request, but the method does not' do
+      it 'returns true when only the current path but not the method matches a dispatch request' do
         env = { 'HTTP_AUTHORIZATION' => 'Bearer 123', 'PATH_INFO' => '/sign_in', 'REQUEST_METHOD' => 'GET' }
         strategy = described_class.new(env, :user)
 
         expect(strategy).to be_valid
       end
 
-      it 'returns true when the current path does not match a dispatch request path' do
-        env = { 'HTTP_AUTHORIZATION' => 'Bearer 123', 'PATH_INFO' => '/users', 'REQUEST_METHOD' => 'POST' }
-        strategy = described_class.new(env, :user)
-
-        expect(strategy).to be_valid
-      end
-    end
-
-    context 'when Authorization header is not valid' do
-      it 'returns false' do
-        env = {}
-        strategy = described_class.new(env, :user)
-
-        expect(strategy).not_to be_valid
-      end
-
-      it 'returns false when the current path matches a dispatch request path' do
-        env = { 'PATH_INFO' => '/sign_in', 'REQUEST_METHOD' => 'POST' }
-        strategy = described_class.new(env, :user)
-
-        expect(strategy).not_to be_valid
-      end
-
-      it 'returns true when the current path does not match a dispatch request path' do
-        env = { 'PATH_INFO' => '/users', 'REQUEST_METHOD' => 'GET' }
-        strategy = described_class.new(env, :user)
-
-        expect(strategy).not_to be_valid
-      end
-    end
-
-    context 'when issuer claim is configured and it matches the configured issuer' do
-      it 'returns true' do
+      it 'returns true when issuer claim is configured and it matches the configured issuer' do
         token = Warden::JWTAuth::TokenEncoder.new.call({ 'iss' => Warden::JWTAuth.config.issuer })
         env = { 'HTTP_AUTHORIZATION' => "Bearer #{token}" }
 
@@ -75,13 +43,20 @@ describe Warden::JWTAuth::Strategy do
 
         expect(strategy).to be_valid
       end
-    end
 
-    context "when issuer claim is configured and it doesn't match the configured issuer" do
-      it 'returns false' do
+      it "returns false when issuer claim is configured and it doesn't match the configured issuer" do
         token = Warden::JWTAuth::TokenEncoder.new.call({ 'iss' => Warden::JWTAuth.config.issuer + 'aaa' })
         env = { 'HTTP_AUTHORIZATION' => "Bearer #{token}" }
 
+        strategy = described_class.new(env, :user)
+
+        expect(strategy).not_to be_valid
+      end
+    end
+
+    context 'when Authorization header is not valid' do
+      it 'returns false' do
+        env = {}
         strategy = described_class.new(env, :user)
 
         expect(strategy).not_to be_valid
